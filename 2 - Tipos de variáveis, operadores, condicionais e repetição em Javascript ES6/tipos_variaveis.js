2533797434e9505aa78e031c36e12ecc97b90063
Thiago Cunha <thiago.santos.cunha@everis.com>
2020-11-13 00:18:44 -0300

    Exercício 2 - Tipos e variáveis

---
+++ 2 - Tipos de variáveis, operadores, condicionais e repetição em Javascript ES6/tipos_variaveis.js
@@ -0,0 +1,17 @@
+const numero = 1;
+const texto = 'Matheus';
+const numeroFlutuante = 1.4;
+const booleano = true;
+let obj = Object.prototype.Pessoa = function () {
+    this.nome, this.idade, this.sexo
+};
+
+const pessoa = new obj.Pessoa();
+pessoa.nome = 'Thiago';
+pessoa.idade = 123;
+
+console.log(numero);
+console.log(texto);
+console.log(numeroFlutuante);
+console.log(booleano);
+console.log(pessoa);
